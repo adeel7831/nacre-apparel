99fdba18cd405970b75918e7ff0cebc95b2ba7f5
Adeel <muhammadadeel7831@gmail.com>
2025-09-08 14:57:56 +0500

    ui of header is completed for all screens

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import Header from "./components/Header";
 function App() {
   return (
-    <>
+    <div className="max-w-[1440px] mx-auto">
       <Header />
       
-    </>
+    </div>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@ import { MdOutlineClose } from "react-icons/md";
 import { RiMenu3Fill } from "react-icons/ri";
 import { Link } from "react-router";
 import MobileMenu from "./MobileMenu";
+import NavItems from "../constants/NavItems";
 
 const Header = () => {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
@@ -11,10 +12,25 @@ const Header = () => {
   };
   return (
     <>
-      <div className="flex justify-between items-center px-3 py-5 bg-amber-300">
-        <Link>Nacre Apparel</Link>
+      <div className="flex justify-between items-center px-3 md:px-8 lg:px-10 xl:px-12 py-5  bg-gray-100">
+        <Link to="/">Nacre Apparel</Link>
 
-        <div onClick={mobileMenuToggler}>
+        <nav className="hidden md:block">
+          <div className="flex items-center gap-5 px-3 py-5">
+            {NavItems.map((item, indx) => {
+              return (
+                <Link
+                  to={item.link}
+                  key={indx}
+                  className=" pr-5 border-r border-gray-300 "
+                >
+                  {item.title}
+                </Link>
+              );
+            })}
+          </div>
+        </nav>
+        <div className="md:hidden" onClick={mobileMenuToggler}>
           {mobileMenuOpen ? (
             <MdOutlineClose className="h-7 w-7" />
           ) : (

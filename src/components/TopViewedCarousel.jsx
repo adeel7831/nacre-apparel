a4a21f8875908cba5e865b5e9866666ca52ca17b
Adeel <muhammadadeel7831@gmail.com>
2025-09-10 13:35:47 +0500

    top viewed carousel added

---
+++ src/components/TopViewedCarousel.jsx
@@ -0,0 +1,62 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/navigation";
+import { Navigation } from "swiper/modules";
+import { GrFormNext, GrFormPrevious } from "react-icons/gr";
+
+import carouselImg1 from "../assets/imgs/heroCarousel1.jpg";
+import carouselImg2 from "../assets/imgs/heroCarousel2.jpg";
+import carouselImg3 from "../assets/imgs/heroCarousel3.jpg";
+
+// 🔹 Array of slides
+const slidesData = [
+  { src: carouselImg1, alt: "Slide 1" },
+  { src: carouselImg2, alt: "Slide 2" },
+  { src: carouselImg3, alt: "Slide 3" },
+];
+
+const TopViewedCarousel = () => {
+  return (
+    <div className="w-full mx-auto relative">
+      <Swiper
+        modules={[Navigation]}
+        navigation={{
+          nextEl: ".custom-next",
+          prevEl: ".custom-prev",
+        }}
+        loop={true}
+        slidesPerView="auto"
+        spaceBetween={10}
+        breakpoints={{
+          640: { spaceBetween: 15 }, // sm
+          768: { spaceBetween: 20 }, // md
+          1024: { spaceBetween: 25 }, // lg
+          1280: { spaceBetween: 30 }, // xl
+        }}
+      >
+        {slidesData.map((slide, index) => (
+          <SwiperSlide
+            key={index}
+            className="!w-[100px] !h-[100px] md:!w-[150px] md:!h-[150px] lg:!w-[200px] lg:!h-[200px] xl:!w-[250px] xl:!h-[250px] overflow-hidden rounded-full"
+          >
+            <img
+              src={slide.src}
+              alt={slide.alt}
+              className="w-full h-full object-cover object-center rounded-full"
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+
+      {/* 🔹 Custom Navigation Buttons */}
+      <button className="custom-prev absolute top-1/2 left-0 z-10 -translate-y-1/2 bg-blue-400 text-white p-1 md:p-2 rounded-full hover:bg-black transition">
+        <GrFormPrevious />
+      </button>
+      <button className="custom-next absolute top-1/2 right-0 z-10 -translate-y-1/2 bg-blue-400 text-white p-1 md:p-2 rounded-full hover:bg-black transition">
+        <GrFormNext />
+      </button>
+    </div>
+  );
+};
+
+export default TopViewedCarousel;
--- src/components/TopViewedSection.jsx
+++ src/components/TopViewedSection.jsx
@@ -1,8 +1,14 @@
+import TopViewedCarousel from "./TopViewedCarousel";
+
 const TopViewedSection = () => {
   return (
     <div className="w-full flex items-center gap-3 md:gap-5 lg:gap-8 xl:gap-10">
-      <h1 className="text-lg md:text-2xl lg:text-4xl xl:text-6xl">Top Viewed</h1>
-      <div></div>
+      <h1 className="w-[20%] text-lg md:text-2xl lg:text-4xl xl:text-6xl">
+        Top Viewed
+      </h1>
+      <div className="w-[80%]">
+        <TopViewedCarousel />
+      </div>
     </div>
   );
 };
